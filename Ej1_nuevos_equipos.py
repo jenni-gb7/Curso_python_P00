c14f6a4789212ab7d2841e4f38a73eae4c43e75d
UNSIJ <gutierrezbetetam95@gmail.com>
2025-03-04 22:06:07 -0600

    listo

---
+++ Ej1_nuevos_equipos.py
@@ -0,0 +1,29 @@
+'''
+Jennifer Marlene Gutierrez Beteta
+13 de enero de 2025.
+Descripcion:
+Ejercicio 2, Parcial 3.
+'''
+
+print("")
+print("*** EQUIPOS PARA EL SEMESTRE ***")
+print("")
+
+Algoritmos_Anarquistas = ["Hector", "Addi","Jesus Alberto"]
+Hackers_de_Cafe = ["Tania", "Patricia", "Rebeca"]
+Codificadores_nocturnos = ["Jamileth", "Bryan", "Rosalinda"]
+Ctrl_Z = ["Galilea", "Jennifer", "Juan"]
+print(Algoritmos_Anarquistas)
+print(Hackers_de_Cafe)
+print(Codificadores_nocturnos)
+print(Ctrl_Z )
+
+Equipo1 = []
+Equipo2 = []
+Equipo3 = []
+Equipo4 = []
+Equipo5 = []
+Equipo6 = []
+Equipo = 0
+
+while equipos
