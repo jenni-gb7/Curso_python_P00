44f7f6dbfdb6e7d3c3f1c753d750fca37cab5fe7
UNSIJ <gutierrezbetetam95@gmail.com>
2025-03-31 21:51:26 -0600

    comentar

---
+++ Codewars/ADNcomplementario.py
@@ -0,0 +1,34 @@
+"""
+Jennifer Marlene Gutiérrez Beteta
+11 de Marzo de 2025.
+Descripción del programa:
+En las cadenas de ADN, los símbolos "A" y "T" son complementos entre sí, como "C" y "G". Su función recibe un lado del ADN (cadena, a excepción de Haskell); Es necesario devolver el otro lado complementario.
+Ejemplo: (entrada --> salida)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"""
+
+
+def DNA_strand(dna):
+
+    texto_convertido = ""
+    for letra in dna:
+        if letra == "A":
+            texto_convertido += "T"
+        elif letra == "T":
+            texto_convertido += "A"
+        elif letra == "C":
+            texto_convertido += "G"
+        elif letra == "G":
+            texto_convertido += "C"
+        else:
+            texto_convertido += letra  # Dejar el carácter sin cambios.
+    return texto_convertido
+
+
+
+
+if __name__ == '__main__':
+    dna = str( input("Ingresa la cadena:"))
+    texto_convertido = DNA_strand(dna)
+    print(texto_convertido)
