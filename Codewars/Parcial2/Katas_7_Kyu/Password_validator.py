4fcaeae100be0371e3fccd16d972be7bf9477647
UNSIJ <gutierrezbetetam95@gmail.com>
2025-06-02 15:14:24 -0600

    no cumple con una condición.

--- Codewars/Parcial2/Katas_7_Kyu/Password_validator.py
+++ Codewars/Parcial2/Katas_7_Kyu/Password_validator.py
@@ -15,20 +15,26 @@ La función toma un argumento de cadena y devuelve si es una contraseña válida
 """
 
 def password(st):
-    if len(st) >= 8:
-        tiene_mayuscula = False
-        tiene_minuscula = False
-        tien_caracteres = False
+    if len(st) < 8:
+        return False
+    tiene_mayuscula = False
+    tiene_minuscula = False
+    tiene_caracteres = False
 
-        for s in st:
-            if s.isupper():
-                tiene_mayuscula = True
-            if s.lower():
-                tiene_minuscula = False
-            if s.isdigit():
-                tienE_caracteres = False
-    else:
-        print("invalido, intenta de nuevo")
+    for s in st:
+        if s.isupper():
+            tiene_mayuscula = True
+        if s.lower():
+            tiene_minuscula = True
+        if s.isdigit():
+            tiene_caracteres = True
+
+    if tiene_mayuscula == False or tiene_minuscula == False or tiene_caracteres == False:
+        return False
+    if tiene_mayuscula == False and tiene_caracteres == False:
+        return False
+    return True
+
 
 if __name__ == '__main__':
     st = str(input("Ingresa la contraseña:"))
